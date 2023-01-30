81ac46d632d3730b195451b505f0aef958256000
Андрей Пшеничников <gr402_pshaal@lab-402-9.tomtit-tomsk.ru>
2023-01-30 09:40:57 +0700

    if else show

--- js/main.js
+++ js/main.js
@@ -4,6 +4,7 @@ let app = new Vue({
         product: "Socks",
         image: "./assets/vmSocks-blue-onWhite.jpg",
         altText: "A pair of socks",
-        link: "https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=socks"
+        inventory: 5,
+        onSale: true
     }
 })
