07422552b9f089223e9155e25048766861899a0b
Андрей Пшеничников <gr402_pshaal@lab-402-9.tomtit-tomsk.ru>
2023-01-30 10:16:58 +0700

    methods, listeners

--- js/main.js
+++ js/main.js
@@ -9,13 +9,28 @@ let app = new Vue({
         variants: [
             {
                 variantId: 2234,
-                variantColor: 'green'
+                variantColor: 'green',
+                variantImage: './assets/vmSocks-green-onWhite.jpg'
             },
             {
                 variantId: 2235,
-                variantColor: 'blue'
+                variantColor: 'blue',
+                variantImage: './assets/vmSocks-blue-onWhite.jpg'
             }
         ],
-        sizes: ['S', 'M', 'L', 'XL', 'XXL', 'XXXL']
+        cart: 0,
+    },
+    methods: {
+        addToCart() {
+            this.cart += 1
+        },
+        deleteFromCart() {
+            if (this.cart > 0) {
+                this.cart -= 1
+            }
+        },
+        updateProduct(variantImage) {
+            this.image = variantImage
+        }
     }
 })
