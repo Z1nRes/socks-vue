18abfc3196dd48d1576d595a43da909d9397734f
Андрей Пшеничников <gr402_pshaal@lab-402-9.tomtit-tomsk.ru>
2023-01-30 09:27:57 +0700

    v-bind

--- js/main.js
+++ js/main.js
@@ -1,6 +1,9 @@
 let app = new Vue({
     el: '#app',
     data: {
-        product: "Socks"
+        product: "Socks",
+        image: "./assets/vmSocks-blue-onWhite.jpg",
+        altText: "A pair of socks",
+        link: "https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=socks"
     }
 })
